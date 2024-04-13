16071e348cb2ad9c91fe244467653502006f65f1
Elias Souza <elias.souza.es609@gmail.com>
2024-04-12 21:33:26 -0300

    bug ao converter os dados para json corrigido

--- src/app.js
+++ src/app.js
@@ -3,14 +3,14 @@ const connect = require('./database/connect');
 const userRoutes = require('./database/controllers/usersController');
 require('dotenv').config();
 
-const sever = express(); 
+const server = express(); 
 
+server.use(express.json())
 // Define o prefixo '/users' para todas as rotas relacionadas aos usuários
-sever.use('/users', userRoutes);
-
+server.use('/users', userRoutes);
 // Chama a função de conexão
 connect();
 
-sever.listen(process.env.PORT, () => {
+server.listen(process.env.PORT, () => {
     console.log('Servidor rodando na porta 3000');
 });

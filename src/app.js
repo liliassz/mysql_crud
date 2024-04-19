067391ff794d374bb12ce1f129279361a3d03f27
Elias Souza <elias.souza.es609@gmail.com>
2024-04-18 21:30:17 -0300

    fixedBug: Erro de cors corrigido

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
 // Importa o módulo express para criação de um servidor web.
 const express = require('express');
 
+//cors 
+const cors = require('cors')
+
+// Ou permitir apenas uma origem específica
+// app.use(cors({ origin: 'http://localhost:5500' }));
+
+
 // Importa a função de conexão com o banco de dados.
 const connect = require('./database/connect');
 
@@ -13,11 +20,22 @@ require('dotenv').config();
 // Cria uma instância do servidor Express.
 const server = express();
 
+// Permitir uma origem específica e métodos HTTP específicos
+// server.use(cors({ 
+//     origin: 'http://localhost:5500',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'] 
+// })); 
+
+// Permitir todas as origens (não recomendado para produção)
+server.use(cors()); 
+
 // Middleware para analisar o corpo das requisições como JSON.
 server.use(express.json());
 
 // Define as rotas relacionadas aos usuários.
 server.use('/users', userRoutes);
+
+server.use(express.static('public'));
 
 // Estabelece a conexão com o banco de dados.
 connect();

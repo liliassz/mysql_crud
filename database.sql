24c9fee4eaf46de7716413f7caea1f3393539218
Elias Souza <elias.souza.es609@gmail.com>
2024-04-26 16:38:45 -0300

    Database Schema

---
+++ database.sql
@@ -0,0 +1,73 @@
+CREATE SCHEMA IF NOT EXISTS `usuarios` DEFAULT CHARACTER SET utf8 ;
+
+CREATE TABLE `user` (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(10) UNIQUE,
+    first_name VARCHAR(10),
+    last_name VARCHAR(10),
+    email VARCHAR(255) UNIQUE ,
+    password VARCHAR(255),
+    age INT,
+    phone VARCHAR(20),
+    gender ENUM('homem', 'mulher', 'outros'),
+    date_of_birth DATE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `address` (
+    user_id INT PRIMARY KEY,
+    street VARCHAR(255),
+    city VARCHAR(255),
+    state VARCHAR(2),
+    postal_code VARCHAR(10),
+    country VARCHAR(50),
+    FOREIGN KEY (user_id) REFERENCES `user`(id) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `social_info` (
+    user_id INT PRIMARY KEY,
+    profile_picture VARCHAR(255),
+    bio TEXT,
+    status ENUM('ativo', 'desativado') DEFAULT 'ativo',
+    website VARCHAR(255),
+    occupation VARCHAR(255),
+    company VARCHAR(255),
+    skill VARCHAR(255),
+    language VARCHAR(255),
+    FOREIGN KEY (user_id) REFERENCES `user`(id) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE VIEW `user_full_details` AS
+SELECT 
+    u.id,
+    u.username,
+    u.first_name,
+    u.last_name,
+    u.email,
+    u.password,
+    u.age,
+    u.phone,
+    u.gender,
+    u.date_of_birth,
+    u.created_at,
+    u.updated_at,
+    a.street,
+    a.city,
+    a.state,
+    a.postal_code,
+    a.country,
+    s.profile_picture,
+    s.bio,
+    s.status,
+    s.website,
+    s.occupation,
+    s.company,
+    s.skill,
+    s.language
+FROM 
+    `user` u
+LEFT JOIN 
+    `address` a ON u.id = a.user_id
+LEFT JOIN 
+    `social_info` s ON u.id = s.user_id;

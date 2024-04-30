1433be5cbdbd54a1bc4b7fbf64cf301f84fac68d
liliassz <elias.souza.es609@gmail.com>
2024-04-30 20:31:28 -0300

    melhoria na sintaxe

--- middlewares/hashPassword.js
+++ middlewares/hashPassword.js
@@ -1,5 +1,12 @@
 const { hash } = require('bcrypt');
 
+/**
+ * Middleware para criar um hash da senha do usuário.
+ * @param {object} req - Objeto de requisição Express.
+ * @param {object} res - Objeto de resposta Express.
+ * @param {function} next - Função de callback para chamar o próximo middleware.
+ * @returns {void}
+ */
 async function hashPassword(req, res, next) {
     try {
         req.body.hashedPassword = await hash(req.body.password, 10);
